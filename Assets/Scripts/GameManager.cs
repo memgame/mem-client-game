e4cc72a2b58de3eebc8e98dd113ab553b3267528
Wenish <jonas.voland@bluewin.ch>
2019-01-21 23:44:45 +0100

    update

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MemClientGame.Assets.Scripts.Network;
+using System;
+
+namespace MemClientGame.Assets.Scripts
+{
+    public class GameManager : MonoBehaviour
+    {
+        private ColyseusClient _colyseusClient;
+        private string cmdInfo = "";
+        // Start is called before the first frame update
+        public async void Start()
+        {
+            string[] arguments = Environment.GetCommandLineArgs();
+                foreach(string arg in arguments)
+                {
+                    cmdInfo += arg.ToString() + "\n ";
+                }
+                Debug.Log(cmdInfo);
+                Debug.Log(GetArg("-projectpath"));
+            /*
+            _colyseusClient = new ColyseusClient("localhost", "8080");
+            await _colyseusClient.ConnectToServer();
+            var room = _colyseusClient.JoinRoom("match");
+            */
+        }
+        private static string GetArg(string name)
+        {
+            var args = System.Environment.GetCommandLineArgs();
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == name && args.Length > i + 1)
+                {
+                    return args[i + 1];
+                }
+            }
+            return null;
+        }
+    }
+}
